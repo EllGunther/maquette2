50ce3660ad618e4d6f668526233112df6de355f8
EllGunther <103885672+EllGunther@users.noreply.github.com>
2022-11-04 00:42:06 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,19 @@
+function sendEmail() {
+    Email.send({
+        SecureToken : "C973D7AD-F097-4B95-91F4-40ABC5567812",
+        To : 'kasongodan24@gmail.com',
+        From : entreprise.value,
+        Subject : objet.value,
+        Body : message.value
+    }).then(
+        message => alert(message)
+    );
+}
 let send = document.getElementById("send");
 send.addEventListener("click", () => {
-    window.open("https://mail.google.com");
+    sendEmail();
 });
+let nom = document.getElementById("nom");
+let objet = document.getElementById("objet");
+let message = document.getElementById("message");
+let entreprise = document.getElementById("entreprise");
